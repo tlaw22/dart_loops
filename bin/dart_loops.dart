85d1fd3e2163c7bfeeb5dd17f6a6cc844a0b6048
tim <timothy.lawless@gmail.com>
2024-01-26 13:14:24 -0500

    lesson complete

--- bin/dart_loops.dart
+++ bin/dart_loops.dart
@@ -3,18 +3,18 @@ import 'package:dart_loops/dart_loops.dart' as dart_loops;
 void main(List<String> arguments) {
   // DRY: Don't repeat yourself.
   // For Loop
-
+  String div = "================================";
   for (int i = 0; i <= 30; i++) {
     if (i % 3 == 0) {
       print(i);
     }
   }
-
+print(div);
   List cities = ["London", "Paris", "Cairo", "Bagdad", "Tampa", "Miami"];
   for (String city in cities) {
     print(city);
   }
-
+print(div);
   // While Loop
   int j = 0;
   while (j <= 20) {
@@ -23,7 +23,7 @@ void main(List<String> arguments) {
     }
     j++;
   }
-
+print(div);
   // Do While Loop
   int g = 0;
   do {
@@ -32,18 +32,28 @@ void main(List<String> arguments) {
     }
     g++;
   } while (g <= 20);
-
+print(div);
   // For Each Loop
   cities.forEach((city) {
     print(city);
   });
 
 // Nested for loops and the break/continue keywords
-  outerLoop:for (int i = 0; i <= 5; i++) {
+  outerLoop:
+  for (int i = 0; i <= 5; i++) {
     for (int j = 0; j <= 5; j++) {
       print("$i $j");
-    } if (i == 2){
+    }
+    if (i == 2) {
       break outerLoop;
+    }
+  }
+  
+
+  print(div);
+  for (int i = 0; i <= 100; i++) {
+    if (i % 6 == 0) {
+      print(i);
     }
   }
 }
